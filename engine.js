f2c1207e8bc8c2309ca282c8c49adc5c6a9d536c
KetyaNop <ketya.n@ligercambodia.org>
2016-05-16 20:06:08 +0700

    Changed effectiveness.js random_events.js engine.js
    Make it so that effectiveness.js also calculate the effect of the random
    event
    
    Make random_event.js be able to calculate its effect
    
    Change function name in engine.js

--- engine.js
+++ engine.js
@@ -2,5 +2,5 @@ function loop(){
   //Modifying sectors’ effectiveness according to current Social Mood
   calculateEffectiveness();
   //Selecting Random Event that instantly influences the game
-  selectSector();//this will return 1 of the event
+  calculateRandomEvent();//this will set eventMoodEffect and eventMoneyEffect to the effect from the random events
 }
